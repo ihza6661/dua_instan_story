209bb438a56f6b6450dfb3f7201f94290243f3a1
ihza6661 <ihzahmahendra6661@gmail.com>
2025-11-18 20:05:42 +0700

    Fix: Disable SSL for MySQL connection to resolve migration error

--- config/database.php
+++ config/database.php
@@ -57,10 +57,7 @@ return [
             'prefix_indexes' => true,
             'strict' => false,
             'engine' => null,
-            'options' => extension_loaded('pdo_mysql') ? array_filter([
-                // PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
-                PDO::MYSQL_ATTR_SSL_KEY => env('MYSQL_ATTR_SSL_KEY'),
-            ]) : [],
+            'options' => extension_loaded('pdo_mysql') ? [] : [],
         ],
 
         'mariadb' => [
