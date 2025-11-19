13709a8b1706363ef5007f6e59ca7dec94fc8ece
ihza6661 <ihzahmahendra6661@gmail.com>
2025-11-19 17:08:47 +0700

    feat: Add new products and update image mappings

--- database/seeders/ProductImageSeeder.php
+++ database/seeders/ProductImageSeeder.php
@@ -42,7 +42,23 @@ class ProductImageSeeder extends Seeder
     private function getProductImagesForProduct(string $productName): array
     {
         $productImageMap = [
-            'Guest Book' => ['guestbook-1/duainsan.story-16-11-2025-0001.jpg', 'guestbook-1/duainsan.story-16-11-2025-0002.jpg', 'guestbook-1/duainsan.story-16-11-2025-0003.jpg', 'guestbook-1/duainsan.story-16-11-2025-0004.jpg', 'guestbook-1/duainsan.story-16-11-2025-0005.jpg', 'guestbook-1/duainsan.story-16-11-2025-0006.jpg', 'guestbook-1/duainsan.story-16-11-2025-0007.jpg', 'guestbook-1/duainsan.story-16-11-2025-0008.jpg'],
+            'Buku Tamu 1' => ['guestbook-1/1.jpg', 'guestbook-1/2.jpg', 'guestbook-1/3.jpg', 'guestbook-1/4.jpg', 'guestbook-1/5.jpg'],
+            'Buku Tamu 2' => ['guestbook-2/1.jpg', 'guestbook-2/2.jpg', 'guestbook-2/3.jpg'],
+            'Buku Tamu 3' => ['guestbook-3/1.jpg', 'guestbook-3/2.jpg', 'guestbook-3/3.jpg', 'guestbook-3/4.jpg', 'guestbook-3/5.jpg', 'guestbook-3/6.jpg'],
+            'Buku Tamu 4' => ['guestbook-4/1.jpg', 'guestbook-4/2.jpg', 'guestbook-4/3.jpg', 'guestbook-4/4.jpg', 'guestbook-4/5.jpg'],
+            'Buku Tamu 5' => ['guestbook-5/1.jpg', 'guestbook-5/2.jpg'],
+            'Buku Tamu 6' => ['guestbook-6/1.jpg', 'guestbook-6/2.jpg', 'guestbook-6/3.jpg', 'guestbook-6/4.jpg'],
+            'Buku Tamu 7' => ['guestbook-7/1.jpg', 'guestbook-7/2.jpg', 'guestbook-7/3.jpg'],
+            'Buku Tamu 8' => ['guestbook-8/1.jpg', 'guestbook-8/2.jpg', 'guestbook-8/3.jpg', 'guestbook-8/4.jpg'],
+            'Produk 1' => ['product-1/1.jpg', 'product-1/2.jpg', 'product-1/3.jpg'],
+            'Produk 2' => ['produk-2/1.jpg', 'produk-2/2.jpg', 'produk-2/3.jpg', 'produk-2/4.jpg'],
+            'Produk 3' => ['produk-3/1.jpg', 'produk-3/2.jpg', 'produk-3/3.jpg', 'produk-3/4.jpg'],
+            'Produk 4' => ['produk-4/1.jpg', 'produk-4/2.jpg', 'produk-4/3.jpg', 'produk-4/4.jpg', 'produk-4/5.jpg'],
+            'Produk 5' => ['produk-5/1.jpg', 'produk-5/2.jpg', 'produk-5/3.jpg', 'produk-5/4.jpg'],
+            'Produk 6' => ['produk-6/1.jpg', 'produk-6/2.jpg', 'produk-6/3.jpg', 'produk-6/4.jpg', 'produk-6/5.jpg'],
+            'Produk 7' => ['produk-7/1.jpg', 'produk-7/2.jpg', 'produk-7/3.jpg', 'produk-7/4.jpg', 'produk-7/5.jpg'],
+            'Produk 8' => ['produk-8/1.jpg', 'produk-8/2.jpg', 'produk-8/3.jpg', 'produk-8/4.jpg'],
+            'Produk 9' => ['produk-9/1.jpg', 'produk-9/2.jpg', 'produk-9/3.jpg', 'produk-9/4.jpg'],
         ];
 
         return $productImageMap[$productName] ?? [];
--- database/seeders/ProductVariantsTableSeeder.php
+++ database/seeders/ProductVariantsTableSeeder.php
@@ -24,10 +24,154 @@ class ProductVariantsTableSeeder extends Seeder
             array (
                 'id' => 1,
                 'product_id' => 1,
-                'price' => 2000,
+                'price' => 10000,
                 'stock' => 100,
                 'created_at' => '2025-11-16 13:29:48',
                 'updated_at' => '2025-11-16 13:29:48',
+            ),
+            1 =>
+            array (
+                'id' => 2,
+                'product_id' => 2,
+                'price' => 10000,
+                'stock' => 50,
+                'created_at' => '2025-11-18 14:00:00',
+                'updated_at' => '2025-11-18 14:00:00',
+            ),
+            2 =>
+            array (
+                'id' => 3,
+                'product_id' => 3,
+                'price' => 10000,
+                'stock' => 75,
+                'created_at' => '2025-11-18 14:00:00',
+                'updated_at' => '2025-11-18 14:00:00',
+            ),
+             3 =>
+            array (
+                'id' => 4,
+                'product_id' => 4,
+                'price' => 1500,
+                'stock' => 100,
+                'created_at' => '2025-11-18 14:00:00',
+                'updated_at' => '2025-11-18 14:00:00',
+            ),
+            4 =>
+            array (
+                'id' => 5,
+                'product_id' => 5,
+                'price' => 10000,
+                'stock' => 100,
+                'created_at' => '2025-11-18 14:00:00',
+                'updated_at' => '2025-11-18 14:00:00',
+            ),
+            5 =>
+            array (
+                'id' => 6,
+                'product_id' => 6,
+                'price' => 10000,
+                'stock' => 100,
+                'created_at' => '2025-11-18 14:00:00',
+                'updated_at' => '2025-11-18 14:00:00',
+            ),
+            6 =>
+            array (
+                'id' => 7,
+                'product_id' => 7,
+                'price' => 10000,
+                'stock' => 100,
+                'created_at' => '2025-11-18 14:00:00',
+                'updated_at' => '2025-11-18 14:00:00',
+            ),
+            7 =>
+            array (
+                'id' => 8,
+                'product_id' => 8,
+                'price' => 10000,
+                'stock' => 100,
+                'created_at' => '2025-11-18 14:00:00',
+                'updated_at' => '2025-11-18 14:00:00',
+            ),
+            8 =>
+            array (
+                'id' => 9,
+                'product_id' => 9,
+                'price' => 10000,
+                'stock' => 100,
+                'created_at' => '2025-11-18 14:00:00',
+                'updated_at' => '2025-11-18 14:00:00',
+            ),
+            9 =>
+            array (
+                'id' => 10,
+                'product_id' => 10,
+                'price' => 1500,
+                'stock' => 100,
+                'created_at' => '2025-11-18 14:00:00',
+                'updated_at' => '2025-11-18 14:00:00',
+            ),
+            10 =>
+            array (
+                'id' => 11,
+                'product_id' => 11,
+                'price' => 1500,
+                'stock' => 100,
+                'created_at' => '2025-11-18 14:00:00',
+                'updated_at' => '2025-11-18 14:00:00',
+            ),
+            11 =>
+            array (
+                'id' => 12,
+                'product_id' => 12,
+                'price' => 1500,
+                'stock' => 100,
+                'created_at' => '2025-11-18 14:00:00',
+                'updated_at' => '2025-11-18 14:00:00',
+            ),
+            12 =>
+            array (
+                'id' => 13,
+                'product_id' => 13,
+                'price' => 1500,
+                'stock' => 100,
+                'created_at' => '2025-11-18 14:00:00',
+                'updated_at' => '2025-11-18 14:00:00',
+            ),
+            13 =>
+            array (
+                'id' => 14,
+                'product_id' => 14,
+                'price' => 1500,
+                'stock' => 100,
+                'created_at' => '2025-11-18 14:00:00',
+                'updated_at' => '2025-11-18 14:00:00',
+            ),
+            14 =>
+            array (
+                'id' => 15,
+                'product_id' => 15,
+                'price' => 1500,
+                'stock' => 100,
+                'created_at' => '2025-11-18 14:00:00',
+                'updated_at' => '2025-11-18 14:00:00',
+            ),
+            15 =>
+            array (
+                'id' => 16,
+                'product_id' => 16,
+                'price' => 1500,
+                'stock' => 100,
+                'created_at' => '2025-11-18 14:00:00',
+                'updated_at' => '2025-11-18 14:00:00',
+            ),
+            16 =>
+            array (
+                'id' => 17,
+                'product_id' => 17,
+                'price' => 1500,
+                'stock' => 100,
+                'created_at' => '2025-11-18 14:00:00',
+                'updated_at' => '2025-11-18 14:00:00',
             ),
         ));
         Schema::enableForeignKeyConstraints();
