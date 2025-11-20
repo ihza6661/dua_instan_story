db4e5f05c49ee91e1b0139be9ed718dbe25aac68
ihza6661 <ihzahmahendra6661@gmail.com>
2025-11-20 21:52:08 +0700

    Rename path column to image in product_images table

--- database/migrations/2025_11_21_000003_create_product_images_table.php
+++ database/migrations/2025_11_21_000003_create_product_images_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->id();
             $table->foreignId('product_id')->constrained()->onDelete('cascade');
             $table->foreignId('product_variant_id')->nullable()->constrained()->onDelete('cascade');
-            $table->string('path');
+            $table->string('image');
             $table->timestamps();
         });
     }
